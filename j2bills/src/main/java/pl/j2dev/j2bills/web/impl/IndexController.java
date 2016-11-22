662a81a8fd3944c2110e0e6a8197c047d39d46ea
Paweł Franczak <gzem89@gmail.com>
2016-11-22 20:34:55 +0100

    clear index controller

--- j2bills/src/main/java/pl/j2dev/j2bills/web/impl/IndexController.java
+++ j2bills/src/main/java/pl/j2dev/j2bills/web/impl/IndexController.java
@@ -1,11 +1,5 @@
 package pl.j2dev.j2bills.web.impl;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,10 +10,6 @@ public class IndexController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String index() {
-
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		System.out.println("ZALOGOWANY JAKO " + authentication.getName());
-
 		return "index";
 	}
 
