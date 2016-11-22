5b2f93d69a33a418369bd00b92e89131c0faa465
Paweł Franczak <gzem89@gmail.com>
2016-11-22 18:42:25 +0100

    working logout

--- j2bills/src/main/java/pl/j2dev/j2bills/config/SecurityConfig.java
+++ j2bills/src/main/java/pl/j2dev/j2bills/config/SecurityConfig.java
@@ -16,7 +16,6 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 	@Autowired
 	DataSource dataSource;
 
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 	    http
@@ -25,9 +24,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 	            .and()
 	        .formLogin()                      
 	            .and()
-	        .httpBasic().and()
-	        	.logout()
-	        		.logoutSuccessUrl("/index");
+	        .httpBasic();
 	    
 	}
 
--- j2bills/src/main/java/pl/j2dev/j2bills/web/impl/IndexController.java
+++ j2bills/src/main/java/pl/j2dev/j2bills/web/impl/IndexController.java
@@ -1,9 +1,11 @@
 package pl.j2dev.j2bills.web.impl;
 
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,19 +13,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class IndexController {
-	
-	@RequestMapping(method=RequestMethod.GET)
+
+	@RequestMapping(method = RequestMethod.GET)
 	public String index() {
-		
+
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		System.out.println("ZALOGOWANY JAKO " + authentication.getName());
-		
+
 		return "index";
 	}
-	
+
 	@RequestMapping(value = "logout", method = RequestMethod.GET)
-	public String logout() {
-		return "logout";
+	public String logout(HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:/";
 	}
-	
+
 }
